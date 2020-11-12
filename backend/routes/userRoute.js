4c7674fff677c1cfd2c4d81b6d0e209689e2366d
emma <enabojohn@gmail.com>
2020-11-12 23:53:04 +0300

    User Authentication and Authorization

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,11 +1,56 @@
 import express from 'express';
 import User from '../models/userModel'
+import { getToken } from '../util';
 
 const router = express.Router();
+//Sign in route
+router.post("/signin", async (req, res) => {
+    const signinUser = await User.findOne({
+        email: req.body.email,
+        password: req.body.password,
+    });
+    if (signinUser) {
+        res.send({
+            _id: signinUser.id,
+            name: signinUser.name,
+            email: signinUser.email,
+            isAdmin: signinUser.isAdmin,
+            token: getToken(signinUser)
+
+        })
+    } else {
+        res.status(401).send({ msg: "Invalid Email or Password" })
+    }
+})
 
 
-router.get("/createadmin", async(req, res) => {
-    try {  
+//Register route
+router.post("/register", async (req, res) => {
+    const user = new User({
+        name: req.body.name,
+        email:req.body.email,
+        password: req.body.password
+    });
+    const newUser = await user.save();
+    //if new user exist
+    if (newUser) {
+        res.send({
+            _id: newUser.id,
+            name: newUser.name,
+            email: newUser.email,
+            isAdmin: newUser.isAdmin,
+            token: getToken(newUser)
+        })
+    } else {
+        res.status(401).send({ msg: "Invalid User Data" })
+    }
+});
+
+
+
+//Create user route
+router.get("/createadmin", async (req, res) => {
+    try {
         const user = new User({
             name: "Polycarp",
             email: "policarp@gmail.com",
