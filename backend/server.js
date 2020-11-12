6d8c67745abe421c168f5a2428d055b7d55d6ce2
emma <enabojohn@gmail.com>
2020-11-12 23:54:10 +0300

    Added App Routings

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import data from './data';
 import dotenv from 'dotenv';
 import config from './config';
 import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 import userRoute from './routes/userRoute'
+import productRoute from './routes/productRoute'
+
 dotenv.config();
 
 //Mongo db connection
@@ -16,21 +18,27 @@ mongoose.connect(mongodbUrl, {
 }).catch(error => console.log(error.reason))
 
 const app = express();
+// Setting up basic middleware for all Express requests
+app.use(bodyParser.json()); // Send JSON responses
+app.use(bodyParser.urlencoded({ extended: false })); // Parses urlencoded bodies
+
+
 
 app.use("/api/users", userRoute)
+app.use("/api/products", productRoute)
 
-app.get("/api/products/", (req, res) => {
-    res.send(data.products);
-});
+// app.get("/api/products/", (req, res) => {
+//     res.send(data.products);
+// });
 
-app.get("/api/products/:id", (req, res) => {
-    const productId = req.params.id;
-    const product = data.products.find(x => x._id == productId);
-    if (product)
-        res.send(product);
-    else
-        res.status(404).send({ msg: "Product Not Found" })
-});
+// app.get("/api/products/:id", (req, res) => {
+//     const productId = req.params.id;
+//     const product = data.products.find(x => x._id == productId);
+//     if (product)
+//         res.send(product);
+//     else
+//         res.status(404).send({ msg: "Product Not Found" })
+// });
 
 app.listen(5000, () => {
     console.log("Server started at http://localhost:5000")
